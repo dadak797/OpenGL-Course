764be0714bd3abda81da2838266fc0d80a9df110
dadak797 <dadak797@gmail.com>
2022-07-18 22:58:47 +0900

    04-01

--- shader/simple.vs
+++ shader/simple.vs
@@ -1,7 +1,10 @@
 #version 330 core
+layout (location = 0) in vec3 aPos;  // 0번째 attribute가 정점의 위치
+layout (location = 1) in vec3 aColor;
 
-layout (location = 0) in vec3 aPos;
+out vec4 vertexColor;
 
 void main() {
     gl_Position = vec4(aPos, 1.0);
+    vertexColor = vec4(aColor, 1.0);
 }
--- src/context.cpp
+++ src/context.cpp
@@ -10,10 +10,10 @@ ContextUPtr Context::Create() {
 
 bool Context::Init() {
     float vertices[] = {
-        0.5f, 0.5f, 0.0f,
-        0.5f,-0.5f, 0.0f,
-        -0.5f,-0.5f, 0.0f,
-        -0.5f, 0.5f, 0.0f
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        0.5f,-0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
     };
 
     uint32_t indices[] = {
@@ -23,9 +23,10 @@ bool Context::Init() {
 
     m_vertexLayout = VertexLayout::Create();
     m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW,
-                     vertices, sizeof(float) * 12);
+                     vertices, sizeof(float) * 24);
 
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER,
                     GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
