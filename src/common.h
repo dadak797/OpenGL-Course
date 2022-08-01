22c629993adaffb217493f4071a484349058eafd
dadak797 <dadak797@gmail.com>
2022-08-01 22:16:51 +0900

    Modify common.h

--- src/common.h
+++ src/common.h
@@ -7,9 +7,8 @@
 #include <optional>
 #ifdef __EMSCRIPTEN__
     #include <emscripten/emscripten.h>
-    //#define GL_GLEXT_PROTOTYPES
-    //#define EGL_EGLEXT_PROTOTYPES
     #define GLFW_INCLUDE_ES3
+    #define GLFW_INCLUDE_GLEXT
 #else  
     #include <glad/glad.h>
 #endif
